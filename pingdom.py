09bb06d432a82492c211e682a3f514feba731fca
Dan Udey <dan@cdslash.net>
2009-08-06 12:45:03 -0700

    Added Apache license boilerplate

--- pingdom.py
+++ pingdom.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
-#
-# Expect this code to be highly derivative of the Twitter python API
-# as found here:
-# http://code.google.com/p/python-twitter/source/browse/trunk/twitter.py?r=177
-#
+
+# Copyright 2009 Dan Udey
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 import time
 import SOAPpy
